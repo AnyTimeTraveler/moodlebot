bea162df25e14b019ee14dac7baa090febf7eb5a
Simon Struck <simon@simonscode.org>
2020-01-05 18:26:11 +0100

    Remove hardcoded moodle address

--- src/main/java/org/simonscode/moodlebot/Main.java
+++ src/main/java/org/simonscode/moodlebot/Main.java
@@ -7,8 +7,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 public class Main {
     public static void main(String[] args) throws TelegramApiRequestException {
-//        MoodleAPI.setMoodleAddresses("https://moodle.hs-emden-leer.de/moodle","moodle.hs-emden-leer.de");
-        MoodleAPI.setMoodleAddresses("http://10.3.14.123", "10.3.14.123");
+        MoodleAPI.setMoodleAddresses(State.instance.moodleAddress, State.instance.moodleHost);
         ApiContextInitializer.init();
         TelegramBotsApi api = new TelegramBotsApi();
         api.registerBot(new Bot());
