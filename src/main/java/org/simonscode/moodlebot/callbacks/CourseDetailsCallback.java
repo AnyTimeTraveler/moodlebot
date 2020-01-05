ee25dd334d8a66c9f0980ace41f83760699e4566
Simon Struck <simon@simonscode.org>
2020-01-06 00:00:21 +0100

    Add section headings for course details

--- src/main/java/org/simonscode/moodlebot/callbacks/CourseDetailsCallback.java
+++ src/main/java/org/simonscode/moodlebot/callbacks/CourseDetailsCallback.java
@@ -9,9 +9,9 @@ import org.simonscode.moodleapi.objects.course.module.*;
 import org.simonscode.moodlebot.State;
 import org.simonscode.moodlebot.UserData;
 import org.simonscode.telegrammenulibrary.Callback;
+import org.simonscode.telegrammenulibrary.ParseMode;
 import org.simonscode.telegrammenulibrary.VerticalMenu;
 import org.telegram.telegrambots.meta.api.methods.ActionType;
-import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendChatAction;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.bots.AbsSender;
@@ -35,33 +35,17 @@ public class CourseDetailsCallback implements Callback {
         }
         final UserData userData = State.instance.users.get(callbackQuery.getFrom().getId());
         VerticalMenu menu = new VerticalMenu();
+        menu.setParseMode(ParseMode.HTML);
 
         menu.setText("<b>" + course.getFullname() + "</b>\n" +
                 "Studenten: " + course.getEnrolledusercount());
         try {
             final CourseContent[] courseDetails = MoodleAPI.getCourseDetails(userData.getToken(), course.getId());
             for (CourseContent courseDetail : courseDetails) {
-                for (Module module : courseDetail.getModules()) {
-                    if (module instanceof LabelModule) {
-                        LabelModule labelModule = (LabelModule) module;
-                        menu.addButton(labelModule.getName(), new TextMenuCallback(this, labelModule.getDescription()));
-                    } else if (module instanceof ForumModule || module instanceof UrlModule) {
-                        menu.addButton(module.getName(), new TextMenuCallback(this, "URL:" + module.getUrl()));
-                    } else if (module instanceof ResourceModule) {
-                        if (((ResourceModule) module).getContents().size() == 1) {
-                            menu.addButton(module.getName(), new DownloadCallback(callbackQuery.getMessage().getChatId(), userData.getToken(), ((ResourceModule) module).getContents().get(0)));
-                        } else {
-                            menu.addButton(module.getName(), new ResourcesMenuCallback(this, (ResourceModule) module));
-                        }
-                    } else if (module instanceof AssignmentModule) {
-                        menu.addButton(module.getName(), new TextMenuCallback(this, module.toString()));
-                    } else if (module instanceof ChoiceGroupModule) {
-                        menu.addButton(module.getName(), new TextMenuCallback(this, module.toString()));
-                    } else if (module instanceof ChoiceModule) {
-                        menu.addButton(module.getName(), new TextMenuCallback(this, module.toString()));
-                    } else {
-                        menu.addButton("Unknown:" + module.getName(), new TextMenuCallback(this, module.toString()));
-                    }
+                if (courseDetail.getModules().size() > 2) {
+                    menu.addButton("Subsection: " + courseDetail.getName(), new CouseModulesCallback(this, courseDetail));
+                } else {
+                    generateCourseModuleCallbacks(callbackQuery, userData, menu, courseDetail);
                 }
             }
         } catch (UnirestException e) {
@@ -70,9 +54,34 @@ public class CourseDetailsCallback implements Callback {
 
         menu.addButton("Go back", coursesCallback);
         try {
-            bot.execute(menu.generateEditMessage(callbackQuery.getMessage()).setParseMode(ParseMode.HTML));
+            bot.execute(menu.generateEditMessage(callbackQuery.getMessage()));
         } catch (TelegramApiException e) {
             e.printStackTrace();
+        }
+    }
+
+    void generateCourseModuleCallbacks(CallbackQuery callbackQuery, UserData userData, VerticalMenu menu, CourseContent courseDetail) {
+        for (Module module : courseDetail.getModules()) {
+            if (module instanceof LabelModule) {
+                LabelModule labelModule = (LabelModule) module;
+                menu.addButton(labelModule.getName(), new TextMenuCallback(this, labelModule.getDescription()));
+            } else if (module instanceof ForumModule || module instanceof UrlModule) {
+                menu.addButton(module.getName(), new TextMenuCallback(this, "URL:" + module.getUrl()));
+            } else if (module instanceof ResourceModule) {
+                if (((ResourceModule) module).getContents().size() == 1) {
+                    menu.addButton(module.getName(), new DownloadCallback(callbackQuery.getMessage().getChatId(), userData.getToken(), ((ResourceModule) module).getContents().get(0)));
+                } else {
+                    menu.addButton(module.getName(), new ResourcesMenuCallback(this, (ResourceModule) module));
+                }
+            } else if (module instanceof AssignmentModule) {
+                menu.addButton(module.getName(), new TextMenuCallback(this, module.toString()));
+            } else if (module instanceof ChoiceGroupModule) {
+                menu.addButton(module.getName(), new TextMenuCallback(this, module.toString()));
+            } else if (module instanceof ChoiceModule) {
+                menu.addButton(module.getName(), new TextMenuCallback(this, module.toString()));
+            } else {
+                menu.addButton("Unknown:" + module.getName(), new TextMenuCallback(this, module.toString()));
+            }
         }
     }
 }
